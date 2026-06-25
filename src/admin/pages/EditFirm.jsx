a0c42fda404ed942a6d3ca75f028f0bb9455c158
Amartya Saran <amartyasaran@gmail.com>
2026-06-25 17:07:10 +0530

    paths fixed

--- src/admin/pages/EditFirm.jsx
+++ src/admin/pages/EditFirm.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import AdminLayout from "../components/AdminLayout";
-import { adminFirmAPI } from "../services/adminApi";
+import { adminFirmAPI } from "../../services/adminApi";
 
 import "./EditFirm.css";
 
